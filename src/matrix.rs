cc35c8ce90d99035db48f47a437b1fd078d8f322
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-02-02 20:06:00 +0000

    matrix macro and a few more operations

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,16 @@
+macro_rules! matrix {
+    ($($($element:expr),*);*) => {
+        Matrix::try_from(vec![$( vec![ $($element),* ] ), *])
+    };
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Matrix {
     height: usize,
     width: usize,
     rows: Vec<Vec<f64>>,
 }
+
 // TODO
 //  Multiply by tupl
 //  Multiply matrices of different size
@@ -66,76 +73,104 @@ impl Matrix {
 
         Ok(Matrix::new(self.height, rows.collect()))
     }
+
+    pub fn transpose(&self) -> Matrix {
+        let columns = (0..self.width).map(|col| self.get_col(col)).collect::<Vec<Vec<_>>>();
+        Matrix::new(self.width, columns)
+    }
+}
+
+impl<A: Into<f64> + Copy> TryFrom<Vec<Vec<A>>> for Matrix {
+    type Error = String;
+
+    fn try_from(value: Vec<Vec<A>>) -> Result<Self, Self::Error> {
+        let height = value.len();
+        let width = value[0].len();
+
+        if width != height {
+            return Err("Not a squae matrix".to_string());
+        }
+
+        let rows = value
+            .iter()
+            .map(|row| row.iter().map(|&x| x.into()).collect())
+            .collect();
+
+        Ok(Matrix::new(height, rows))
+    }
 }
 
 #[cfg(test)]
 mod matrix_test {
     use super::*;
+    #[test]
+    fn macro_test() {
+        let m1 = matrix![
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4]
+        .unwrap();
+
+        let m2 = Matrix::new(
+            4,
+            vec![
+                vec![1.0, 2.0, 3.0, 4.0],
+                vec![1.0, 2.0, 3.0, 4.0],
+                vec![1.0, 2.0, 3.0, 4.0],
+                vec![1.0, 2.0, 3.0, 4.0],
+            ],
+        );
+
+        assert_eq!(m1, m2);
+    }
 
     // TODO: Make a macro to make matrix easier...
     #[test]
     fn matrix_multiply() {
-        let m1 = Matrix::new(
-            4,
-            vec![
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-            ],
-        );
+        let m1 = matrix![
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4]
+        .unwrap();
 
-        let m2 = Matrix::new(
-            4,
-            vec![
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![1.0, 2.0, 3.0, 4.0],
-            ],
-        );
+        let m2 = matrix![
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4;
+            1, 2, 3, 4]
+        .unwrap();
 
-        let m3 = Matrix::new(
-            4,
-            vec![
-                vec![10.0, 20.0, 30.0, 40.0],
-                vec![10.0, 20.0, 30.0, 40.0],
-                vec![10.0, 20.0, 30.0, 40.0],
-                vec![10.0, 20.0, 30.0, 40.0],
-            ],
-        );
+        let m3 = matrix![
+            10, 20, 30, 40;
+            10, 20, 30, 40;
+            10, 20, 30, 40;
+            10, 20, 30, 40]
+        .unwrap();
 
         assert_eq!(m1.times(m2).unwrap(), m3);
 
-        let m1 = Matrix::new(
-            4,
-            vec![
-                vec![1.0, 2.0, 3.0, 4.0],
-                vec![5.0, 6.0, 7.0, 8.0],
-                vec![9.0, 8.0, 7.0, 6.0],
-                vec![5.0, 4.0, 3.0, 2.0],
-            ],
-        );
+        let m1 = matrix![
+            1, 2, 3, 4;
+            5, 6, 7, 8;
+            9, 8, 7, 6;
+            5, 4, 3, 2]
+        .unwrap();
 
-        let m2 = Matrix::new(
-            4,
-            vec![
-                vec![-2.0, 1.0, 2.0, 3.0],
-                vec![3.0, 2.0, 1.0, -1.0],
-                vec![4.0, 3.0, 6.0, 5.0],
-                vec![1.0, 2.0, 7.0, 8.0],
-            ],
-        );
+        let m2 = matrix![
+            -2, 1, 2, 3;
+            3, 2, 1, -1;
+            4, 3, 6, 5;
+            1, 2, 7, 8]
+        .unwrap();
 
-        let m3 = Matrix::new(
-            4,
-            vec![
-                vec![20.0, 22.0, 50.0, 48.0],
-                vec![44.0, 54.0, 114.0, 108.0],
-                vec![40.0, 58.0, 110.0, 102.0],
-                vec![16.0, 26.0, 46.0, 42.0],
-            ],
-        );
+        let m3 = matrix![
+            20, 22, 50, 48;
+            44, 54, 114, 108;
+            40, 58, 110, 102;
+            16, 26, 46, 42]
+        .unwrap();
 
         assert_eq!(m1.times(m2).unwrap(), m3);
     }
@@ -143,18 +178,48 @@ mod matrix_test {
     #[test]
     fn identity() {
         let id2 = Matrix::identity(2);
-        let m2x2 = Matrix::new(2, vec![vec![1.0, 0.0], vec![0.0, 1.0]]);
+        let m2x2 = matrix![
+            1, 0; 
+            0, 1]
+        .unwrap();
+
         assert_eq!(id2, m2x2);
 
         let id3 = Matrix::identity(3);
-        let m3x3 = Matrix::new(
-            3,
-            vec![
-                vec![1.0, 0.0, 0.0],
-                vec![0.0, 1.0, 0.0],
-                vec![0.0, 0.0, 1.0],
-            ],
-        );
+        let m3x3 = matrix![
+            1, 0, 0;
+            0, 1, 0;
+            0, 0, 1]
+        .unwrap();
+
         assert_eq!(id3, m3x3);
+
+        let m4 = matrix![
+            20, 22, 50, 48;
+            44, 54, 114, 108;
+            40, 58, 110, 102;
+            16, 26, 46, 42]
+        .unwrap();
+
+        assert_eq!(m4.times(Matrix::identity(4)).unwrap(), m4);
+    }
+
+    #[test]
+    fn transpose() {
+        let m = matrix![
+            0, 9, 3, 0; 
+            9, 8, 0, 8;
+            1, 8, 5, 3; 
+            0, 0, 4, 8]
+        .unwrap();
+
+        let t = matrix![
+            0, 9, 1, 0;
+            9, 8, 8, 0; 
+            3, 0, 5, 4; 
+            0, 8, 3, 8]
+        .unwrap();
+
+        assert_eq!(m.transpose(), t);
     }
 }
