a4c028d8efa6f11b6f61ee2cd475cc3168cb3ff2
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-02-07 22:31:28 +0000

    Limit to Squre Matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,67 +1,82 @@
+use core::panic;
+
 macro_rules! matrix {
     ($($($element:expr),*);*) => {
-        Matrix::try_from(vec![$( vec![ $($element),* ] ), *])
+        SquareMatrix::from(vec![$( vec![ $($element),* ] ), *])
     };
 }
 
+// Can we always know the size at compile time ? If so, we can make this a bit nicer
 #[derive(Debug, PartialEq)]
-pub struct Matrix {
-    height: usize,
-    width: usize,
-    rows: Vec<Vec<f64>>,
+struct SquareMatrix {
+    pub size: usize,
+    pub data: Vec<Vec<f64>>,
 }
 
-// TODO
-//  Multiply by tupl
-//  Multiply matrices of different size
-impl Matrix {
-    /// Make a new square matrix
-    pub fn new(size: usize, rows: Vec<Vec<f64>>) -> Self {
-        Self {
-            height: size,
-            width: size,
-            rows,
+impl<A: Into<f64> + Copy> From<Vec<Vec<A>>> for SquareMatrix {
+    fn from(data: Vec<Vec<A>>) -> Self {
+        let height = data.len();
+        let width = data[0].len();
+
+        if height != width {
+            panic!("Not a square matrix!");
         }
+
+        for row in &data {
+            if row.len() != width {
+                panic!("Not all rows are of the same length!");
+            }
+        }
+
+        SquareMatrix {
+            size: height,
+            data: data
+                .iter()
+                .map(|row| row.iter().map(|&elem| elem.into()).collect())
+                .collect(),
+        }
+    }
+}
+
+impl SquareMatrix {
+    pub fn new(size: usize, rows: Vec<Vec<f64>>) -> Self {
+        Self { size, data: rows }
     }
 
     pub fn identity(size: usize) -> Self {
-        let mut rows = vec![vec![0.0; size]; size];
+        let mut data = vec![vec![0.0; size]; size];
         for i in 0..size {
-            rows[i][i] = 1.0;
+            data[i][i] = 1.0;
         }
 
-        Self {
-            height: size,
-            width: size,
-            rows,
-        }
+        Self { size, data }
     }
 
     /// # Panics
     ///
     /// Will panic if i is not in range
     pub fn get_row(&self, i: usize) -> Vec<f64> {
-        self.rows[i].clone()
+        self.data[i].clone()
     }
 
     /// # Panics
     ///
     /// Will panic if i is not in range
     pub fn get_col(&self, i: usize) -> Vec<f64> {
-        self.rows.iter().map(|row| row[i]).collect::<Vec<f64>>()
+        self.data.iter().map(|row| row[i]).collect::<Vec<f64>>()
     }
 
     /// Try to multiply two matrices
     ///
     /// Will fail when matrices are not the same size
-    pub fn times(&self, rhs: Matrix) -> Result<Matrix, String> {
-        if self.height != rhs.height || self.width != rhs.width {
-            return Err("Matrices are not the same size".to_string());
+    pub fn times(&self, rhs: SquareMatrix) -> Option<SquareMatrix> {
+        if self.size != rhs.size {
+            return None;
         }
 
-        let rows = (0..self.height).map(|row_index| {
+        let rows = (0..self.size).map(|row_index| {
             let row = self.get_row(row_index);
-            (0..self.width)
+            (0..self.size)
                 .map(|col_index| {
                     row.iter()
                         .zip(rhs.get_col(col_index))
@@ -71,32 +86,43 @@ impl Matrix {
                 .collect()
         });
 
-        Ok(Matrix::new(self.height, rows.collect()))
+        Some(SquareMatrix::new(self.size, rows.collect()))
     }
 
-    pub fn transpose(&self) -> Matrix {
-        let columns = (0..self.width).map(|col| self.get_col(col)).collect::<Vec<Vec<_>>>();
-        Matrix::new(self.width, columns)
-    }
-}
-
-impl<A: Into<f64> + Copy> TryFrom<Vec<Vec<A>>> for Matrix {
-    type Error = String;
-
-    fn try_from(value: Vec<Vec<A>>) -> Result<Self, Self::Error> {
-        let height = value.len();
-        let width = value[0].len();
-
-        if width != height {
-            return Err("Not a squae matrix".to_string());
+    fn times_tuple(&self, tuple: Vec<f64>) -> Option<Vec<f64>> {
+        if self.size != tuple.len() {
+            return None;
         }
 
-        let rows = value
-            .iter()
-            .map(|row| row.iter().map(|&x| x.into()).collect())
-            .collect();
+        Some(
+            self.data
+                .iter()
+                .map(|row| row.iter().zip(&tuple).map(|(a, b)| a * b).sum())
+                .collect(),
+        )
+    }
 
-        Ok(Matrix::new(height, rows))
+    pub fn transpose(&self) -> SquareMatrix {
+        let columns = (0..self.size)
+            .map(|col| self.get_col(col))
+            .collect::<Vec<Vec<_>>>();
+        SquareMatrix::new(self.size, columns)
+    }
+
+    pub fn times_vec(
+        &self,
+        vec: crate::points::vector::Vector,
+    ) -> Option<crate::points::vector::Vector> {
+        let new = self.times_tuple(vec![vec.x, vec.y, vec.z])?;
+        Some(crate::points::vector::Vector::new(new[0], new[1], new[2]))
+    }
+
+    pub fn times_coord(
+        &self,
+        coord: crate::points::coord::Coord,
+    ) -> Option<crate::points::coord::Coord> {
+        let new = self.times_tuple(vec![coord.x, coord.y, coord.z])?;
+        Some(crate::points::coord::Coord::new(new[0], new[1], new[2]))
     }
 }
 
@@ -109,10 +135,9 @@ mod matrix_test {
             1, 2, 3, 4;
             1, 2, 3, 4;
             1, 2, 3, 4;
-            1, 2, 3, 4]
-        .unwrap();
+            1, 2, 3, 4];
 
-        let m2 = Matrix::new(
+        let m2 = SquareMatrix::new(
             4,
             vec![
                 vec![1.0, 2.0, 3.0, 4.0],
@@ -125,29 +150,25 @@ mod matrix_test {
         assert_eq!(m1, m2);
     }
 
-    // TODO: Make a macro to make matrix easier...
     #[test]
     fn matrix_multiply() {
         let m1 = matrix![
             1, 2, 3, 4;
             1, 2, 3, 4;
             1, 2, 3, 4;
-            1, 2, 3, 4]
-        .unwrap();
+            1, 2, 3, 4];
 
         let m2 = matrix![
             1, 2, 3, 4;
             1, 2, 3, 4;
             1, 2, 3, 4;
-            1, 2, 3, 4]
-        .unwrap();
+            1, 2, 3, 4];
 
         let m3 = matrix![
             10, 20, 30, 40;
             10, 20, 30, 40;
             10, 20, 30, 40;
-            10, 20, 30, 40]
-        .unwrap();
+            10, 20, 30, 40];
 
         assert_eq!(m1.times(m2).unwrap(), m3);
 
@@ -155,42 +176,37 @@ mod matrix_test {
             1, 2, 3, 4;
             5, 6, 7, 8;
             9, 8, 7, 6;
-            5, 4, 3, 2]
-        .unwrap();
+            5, 4, 3, 2];
 
         let m2 = matrix![
             -2, 1, 2, 3;
             3, 2, 1, -1;
             4, 3, 6, 5;
-            1, 2, 7, 8]
-        .unwrap();
+            1, 2, 7, 8];
 
         let m3 = matrix![
             20, 22, 50, 48;
             44, 54, 114, 108;
             40, 58, 110, 102;
-            16, 26, 46, 42]
-        .unwrap();
+            16, 26, 46, 42];
 
         assert_eq!(m1.times(m2).unwrap(), m3);
     }
 
     #[test]
     fn identity() {
-        let id2 = Matrix::identity(2);
+        let id2 = SquareMatrix::identity(2);
         let m2x2 = matrix![
-            1, 0; 
-            0, 1]
-        .unwrap();
+            1, 0;
+            0, 1];
 
         assert_eq!(id2, m2x2);
 
-        let id3 = Matrix::identity(3);
+        let id3 = SquareMatrix::identity(3);
         let m3x3 = matrix![
             1, 0, 0;
             0, 1, 0;
-            0, 0, 1]
-        .unwrap();
+            0, 0, 1];
 
         assert_eq!(id3, m3x3);
 
@@ -198,27 +214,24 @@ mod matrix_test {
             20, 22, 50, 48;
             44, 54, 114, 108;
             40, 58, 110, 102;
-            16, 26, 46, 42]
-        .unwrap();
+            16, 26, 46, 42];
 
-        assert_eq!(m4.times(Matrix::identity(4)).unwrap(), m4);
+        assert_eq!(m4.times(SquareMatrix::identity(4)).unwrap(), m4);
     }
 
     #[test]
     fn transpose() {
         let m = matrix![
-            0, 9, 3, 0; 
+            0, 9, 3, 0;
             9, 8, 0, 8;
-            1, 8, 5, 3; 
-            0, 0, 4, 8]
-        .unwrap();
+            1, 8, 5, 3;
+            0, 0, 4, 8];
 
         let t = matrix![
             0, 9, 1, 0;
-            9, 8, 8, 0; 
-            3, 0, 5, 4; 
-            0, 8, 3, 8]
-        .unwrap();
+            9, 8, 8, 0;
+            3, 0, 5, 4;
+            0, 8, 3, 8];
 
         assert_eq!(m.transpose(), t);
     }
