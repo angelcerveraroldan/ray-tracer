5c91d235702de38bf0fbaef37a3875b750493a12
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-07-29 22:27:30 +0100

    Test Sphere transformation

--- tracer/src/shapes/sphere.rs
+++ tracer/src/shapes/sphere.rs
@@ -28,7 +28,7 @@ impl Hittable for Sphere {
 #[cfg(test)]
 mod test_hittable_sphere {
     use std::vec;
-
+    use crate::point::coord::Coord;
     use super::*;
 
     #[test]
@@ -64,5 +64,12 @@ mod test_hittable_sphere {
         let ray = crate::ray::Ray::from(((0, 0, 5), (0, 0, 1)));
         let hits = Sphere::default().hit_times(&ray);
         assert_eq!(hits, vec![-6.0, -4.0]);
+    }
+
+    #[test]
+    fn transform_translate() {
+        let mut sphere = Sphere::default();
+        sphere.transformation.translate((2, 3, 4));
+        assert_eq!(sphere.transformation.matrix, TransformationMatrix::translation(Coord::from((2, 3, 4))));
     }
 }
